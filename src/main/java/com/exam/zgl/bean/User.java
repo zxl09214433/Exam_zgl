3efd34adcef1fc6e691a4f8227c7a4ab53b3bd21
zxl_global <2253159155_global@qq.com>
2019-07-28 17:57:21 +0800

    开发者代码提交

--- src/main/java/com/exam/zgl/bean/User.java
+++ src/main/java/com/exam/zgl/bean/User.java
@@ -4,6 +4,7 @@ public class User {
 	public static void main(String[] args) {
 		System.out.println("我是项目经理.第一次更新代码");
 		System.out.println("2222222222222");
+		System.out.println("我是开发人员我提交一次代码");
 	}
 
 }
