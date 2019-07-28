856b1556b4a3dcc120c968db3f764212ec3de6a4
guoliangzhang <guoliangzhang@qq.com>
2019-07-28 19:13:36 +0800

    项目经理第二次提交

--- src/main/java/com/exam/zgl/bean/User.java
+++ src/main/java/com/exam/zgl/bean/User.java
@@ -5,6 +5,7 @@ public class User {
 		System.out.println("我是项目经理.第一次更新代码");
 		System.out.println("2222222222222");
 		System.out.println("我是开发人员我提交一次代码");
+		System.out.println("我是项目经理第二次提交项目");
 	}
 
 }
